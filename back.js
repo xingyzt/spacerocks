b59d25fc5eeced95f122e341c7542630135f00c5
XYZt <31298876+FlyOrBoom@users.noreply.github.com>
2019-07-29 16:21:14 -0700

    back arrow

---
+++ back.js
@@ -0,0 +1,14 @@
+function back(){
+  document.getElementById('blank').classList='opaque';
+  var d = new Date();
+  d = d.getTime();
+  var i = setInterval(
+    function(){
+      var v = new Date();
+      v.getTime();
+      if((v-d)>500){
+        clearInterval(i);
+        window.location='/';
+      }
+    },10);
+}
